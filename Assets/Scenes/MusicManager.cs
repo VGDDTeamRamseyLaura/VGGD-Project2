8a9ea5a7dc7e8e06ce3c88b0b26cb45f2e3a4fda
ramseymardini <ramseymardini@gmail.com>
2020-10-01 11:55:33 -0700

    Added air jump pickup and scaffolding for shrinking + sounds

---
+++ Assets/Scenes/MusicManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    private List<AudioSource> music;
+
+    [SerializeField]
+    private int timeToPhase;
+    #endregion
+
+    #region Private Variables
+    private AudioSource currentlyPlaying;
+    #endregion
+
+    private static GameObject musicManager;
+
+    private void Awake()
+    {
+        if (musicManager == null)
+        {
+            musicManager = gameObject;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+        music[0].Play();
+        currentlyPlaying = music[0];
+    }
+}
---
+++ Assets/ShrinkLaser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkLaser : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    private float timeToShrink;
+
+    [SerializeField]
+    private float shrinkFactor;
+    #endregion
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            StartCoroutine(ShrinkObject(other.gameObject));
+        }
+    }
+
+    private IEnumerator ShrinkObject(GameObject objectToShrink)
+    {
+        objectToShrink.transform.localScale = new Vector2(0.5f, 0.5f);
+        yield return null;
+    }
+}
